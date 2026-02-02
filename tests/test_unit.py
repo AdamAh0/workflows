e6aa58d72b606fa776fc556f926ec867d3ff3c31
AdamAh0 <adam.ahmani11@gmail.com>
2026-02-02 16:31:43 +0100

    best commit

---
+++ tests/test_unit.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from application.main import app
+import pytest
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == "Hello World!"
+
+def test_square():
+    test_number = 4
+    test_expected_result = 16
+
+    res = client.get(f"/{test_number}")
+
+    assert res.status_code == 200
+    assert res.json() == test_expected_result
+
+
+import math
+from random import randint
+
+lists_of_numbers = [randint(1, 100) for i in range(10)]
+
+@pytest.mark.parametrize("number", lists_of_numbers)
+def test_return_square_twice(number : int):
+    response = client.get(f"/twice/{number}")
+    assert response.status_code == 200
+    assert response.json() == math.pow(number, 2) * 2
